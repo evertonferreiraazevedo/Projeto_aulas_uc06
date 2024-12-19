22e0e78c78b9823ea0fa5ef784aaa94d29088037
Aluno <aluno@alunos.ce.senac.br>
2024-12-19 20:10:26 -0300

    commit 19/12/2024

--- meu_projeto/meu_app/urls.py
+++ meu_projeto/meu_app/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('endereco/', views.endereco, name='endereco'),
     path('blog/', views.blog, name='blog'),
     path('parceiros/', views.parceiros, name='parceiros'),
-    path('cadastrar_cliente/', views.cadastrar_cliente, name='cadastrar_cliente')  
+    path('cadastrar_cliente/', views.cadastrar_cliente, name='cadastrar_cliente'),
+    path('cadastrar_produto/', views.cadastrar_produto, name='cadastrar_produto'),
+    path('cadastrar_pedido/', views.cadastrar_pedido, name='cadastrar_pedido'),
 ]
 
 
--- meu_projeto/meu_app/views.py
+++ meu_projeto/meu_app/views.py
@@ -24,17 +24,65 @@ def parceiros(request):
 def cadastrar_cliente(request):
     if request.method == 'POST':
         nome = request.POST.get('nome')
-        print(nome)
         telefone = request.POST.get('telefone')
-        print(telefone)
         email = request.POST.get('email')
-        print(email)
-        Cliente.objects.create(nome=nome, telefone=telefone, email=email)
-
-        # if nome and telefone and email:
-        #     Cliente.objects.create(nome=nome, telefone=telefone, email=email)
-        #     return HttpResponse("Cliente cadastrado com sucesso!")  
-        # else:
-        #     return HttpResponse("Erro: Campos obrigatórios não preenchidos.") 
-
+        
+        # Verifica se o e-mail já está no banco
+        if Cliente.objects.filter(email=email).exists():
+            return HttpResponse("Erro: Este e-mail já está cadastrado!")  # Mensagem de erro amigável
+        
+        try:
+            Cliente.objects.create(nome=nome, telefone=telefone, email=email)
+            return HttpResponse("Cliente cadastrado com sucesso! <a href='/cadastrar_cliente/'>Cadastrar outro cliente</a>")
+        except IntegrityError:
+            return HttpResponse("Erro ao cadastrar cliente. Verifique os dados informados. <a href='/cadastrar_cliente/'>Tente Novamente</a>")
+    
     return render(request, 'cadastro_cliente.html')
+
+# Cadastro de Produto
+def cadastrar_produto(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        quantidade = request.POST.get('quantidade')
+
+        if nome and quantidade:
+            try:
+                quantidade = int(quantidade)  # Conversão para inteiro
+                Produto.objects.create(nome=nome, quantidade=quantidade)
+                return HttpResponse("Produto cadastrado com sucesso! <a href='/cadastrar_produto/'>Cadastrar outro produto</a>")
+            except ValueError:
+                return HttpResponse("Erro: A quantidade deve ser um número inteiro. <a href='/cadastrar_produto/'>Tentar novamente</a>")
+        else:
+            return HttpResponse("Erro: Todos os campos são obrigatórios. <a href='/cadastrar_produto/'>Tentar novamente</a>")
+    
+    return render(request, 'cadastrar_produto.html')
+
+# Cadastro de Pedido
+def cadastrar_pedido(request):
+    clientes = Cliente.objects.all()
+    produtos = Produto.objects.all()
+
+    if request.method == 'POST':
+        cliente_id = request.POST.get('cliente')
+        produto_id = request.POST.get('produto')
+        quantidade = request.POST.get('quantidade')
+
+        if cliente_id and produto_id and quantidade:
+            try:
+                cliente = Cliente.objects.get(id=cliente_id)
+                produto = Produto.objects.get(id=produto_id)
+                quantidade = int(quantidade)  # Conversão para inteiro
+
+                # Criação do pedido
+                Pedido.objects.create(cliente=cliente, produto=produto, quantidade=quantidade)
+                return HttpResponse("Pedido cadastrado com sucesso! <a href='/cadastrar_pedido/'>Cadastrar outro pedido</a>")
+            except Cliente.DoesNotExist:
+                return HttpResponse("Erro: Cliente não encontrado. <a href='/cadastrar_pedido/'>Tentar novamente</a>")
+            except Produto.DoesNotExist:
+                return HttpResponse("Erro: Produto não encontrado. <a href='/cadastrar_pedido/'>Tentar novamente</a>")
+            except ValueError:
+                return HttpResponse("Erro: A quantidade deve ser um número inteiro. <a href='/cadastrar_pedido/'>Tentar novamente</a>")
+        else:
+            return HttpResponse("Erro: Todos os campos são obrigatórios. <a href='/cadastrar_pedido/'>Tentar novamente</a>")
+    
+    return render(request, 'cadastrar_pedido.html', {'clientes': clientes, 'produtos': produtos})
